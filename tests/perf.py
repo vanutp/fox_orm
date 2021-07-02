e60c862367557f77bced340e8516755979624d52
vanutp <hello@vanutp.dev>
2021-07-03 01:45:02 +0300

    Fix perf.py

--- tests/perf.py
+++ tests/perf.py
@@ -5,7 +5,7 @@ from time import time
 from sqlalchemy import create_engine
 
 from fox_orm import FoxOrm
-from tests.models import metadata, a, A
+from tests.models import A
 
 DB_FILE = 'test.db'
 DB_URI = 'sqlite:///test.db'
@@ -13,16 +13,16 @@ DB_URI = 'sqlite:///test.db'
 if os.path.exists(DB_FILE):
     os.remove(DB_FILE)
 FoxOrm.init(DB_URI)
-metadata.create_all(create_engine(DB_URI))
+FoxOrm.metadata.create_all(create_engine(DB_URI))
 
-ITERATIONS = 100
+ITERATIONS = 300
 
 
 async def main():
     print('Simple insert')
     time_start = time()
     for i in range(ITERATIONS):
-        await FoxOrm.db.execute(a.insert(), {
+        await FoxOrm.db.execute(A.__table__.insert(), {
             'text': 'test',
             'n': i,
         })
@@ -38,12 +38,12 @@ async def main():
 
     time_start = time()
     for i in range(ITERATIONS):
-        data = await FoxOrm.db.fetch_all(a.select().where(a.c.text == 'test'))
+        data = await FoxOrm.db.fetch_all(A.__table__.select().where(A.__table__.c.text == 'test'))
     print('- Databases', (time() - time_start) / ITERATIONS)
 
     time_start = time()
     for i in range(ITERATIONS):
-        data = await A.select_all(A.c.text == 'test2')
+        data = await A.select_all(A.c.text == 'test2', skip_parsing=True)
     print('- FoxOrm', (time() - time_start) / ITERATIONS)
 
 
