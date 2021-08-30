d7c5c823a52b1b3c4059255da70ed30e6ccae2a2
vanutp <hello@vanutp.dev>
2021-08-30 21:01:27 +0300

    [version] 0.3.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='fox-orm',
-    version='0.3.2',
+    version='0.3.2.1',
     author='vanutp',
     author_email='hello@vanutp.dev',
     description='Simple pydantic & databases based orm',
